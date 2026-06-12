9c0e8e03b09e31da216bd3c4820cb83d18949f32
Redish03 <pluto0303@pukyong.ac.kr>
2026-06-12 17:46:26 +0900

    ui: DestinationSelectionScreen에 safeContentPadding 추가

--- nureongi/shared/src/commonMain/kotlin/com/woowa/nureongi/ui/screen/DestinationSelectionScreen.kt
+++ nureongi/shared/src/commonMain/kotlin/com/woowa/nureongi/ui/screen/DestinationSelectionScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -72,6 +73,7 @@ private fun DestinationSelectionContent(
         modifier = modifier
             .fillMaxSize()
             .background(NureongiColors.Background)
+            .safeContentPadding()
             .padding(20.dp),
     ) {
         Column(
